ce23493bf2bde081706cc6efac36fd0372e0f847
David Rohnow <davidrohnow@gmail.com>
2024-01-13 22:15:13 -0800

    fix indent

--- checkEndpoint.py
+++ checkEndpoint.py
@@ -24,6 +24,8 @@ def endPointException():
     print('...please try again.')
 
 
+
+
 # def checks https:// and http:// endpoints; takes response and returns infornamtion about http status code and site status.
 def checkEndpoint(endPoint, httpStatusCode):
     try:
@@ -79,45 +81,45 @@ def checkEndpoint(endPoint, httpStatusCode):
                 # response to user that endpoint does not exist.
                 print('not able to check SSL certificate expiry; the endpoint may be misspelled or does not exist.')
 
-    # def returns ssl expire date
-    def ssl_expiry_datetime(hostname: str) -> datetime.datetime:
-        ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+# def returns ssl expire date
+def ssl_expiry_datetime(hostname: str) -> datetime.datetime:
+    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
 
-        context = ssl.create_default_context()
-        conn = context.wrap_socket(
-            socket.socket(socket.AF_INET),
-            server_hostname=hostname,
+    context = ssl.create_default_context()
+    conn = context.wrap_socket(
+        socket.socket(socket.AF_INET),
+        server_hostname=hostname,
     )
 
-        conn.connect((hostname, 443))
-        ssl_info = conn.getpeercert()
-        return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+    conn.connect((hostname, 443))
+    ssl_info = conn.getpeercert()
+    return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
 
-    # def to let user know when the SSL certificate will expire if within 30 days
-    def ssl_valid_time_remaining(hostname: str) -> datetime.timedelta:
-        """Get the number of days left in a cert's lifetime."""
-        expires = ssl_expiry_datetime(hostname)
+# def to let user know when the SSL certificate will expire if within 30 days
+def ssl_valid_time_remaining(hostname: str) -> datetime.timedelta:
+    """Get the number of days left in a cert's lifetime."""
+    expires = ssl_expiry_datetime(hostname)
 
-        return expires - datetime.datetime.utcnow()
+    return expires - datetime.datetime.utcnow()
 
-    # def checks SSL certificate status
-    def test_host(hostname: str, buffer_days: int=30) -> str:
-        """Return test message for hostname cert expiration."""
-        try:
-            will_expire_in = ssl_valid_time_remaining(hostname)
-        except ssl.CertificateError as e:
+# def checks SSL certificate status
+def test_host(hostname: str, buffer_days: int=30) -> str:
+    """Return test message for hostname cert expiration."""
+    try:
+        will_expire_in = ssl_valid_time_remaining(hostname)
+    except ssl.CertificateError as e:
+        return f'{hostname} cert error {e}'
+    except ssl.SSLError as e:
             return f'{hostname} cert error {e}'
-        except ssl.SSLError as e:
-            return f'{hostname} cert error {e}'
-        except socket.timeout as e:
+    except socket.timeout as e:
             return f'{hostname} could not connect'
+    else:
+        if will_expire_in < datetime.timedelta(days=0):
+            return f'{hostname} cert will expired'
+        elif will_expire_in < datetime.timedelta(days=buffer_days):
+            return f'{hostname} cert will expire in {will_expire_in}'
         else:
-            if will_expire_in < datetime.timedelta(days=0):
-                return f'{hostname} cert will expired'
-            elif will_expire_in < datetime.timedelta(days=buffer_days):
-                return f'{hostname} cert will expire in {will_expire_in}'
-            else:
-                return f'{hostname} cert is fine'
+            return f'{hostname} cert is fine'
 
 
 # entry point into program
